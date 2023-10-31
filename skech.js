4d075f08d614804a12c195a2acde0d83b9683bd9
eric <148893165+ericsilvaamarante@users.noreply.github.com>
2023-10-31 13:52:08 -0300

    adicionando os arquivos html.js e css

---
+++ skech.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400)  
+    background('#03A9F4');
+  
+  }
+  
+  function draw() {
+    stroke('pink')
+    fill('red')
+   
+  
+  if(mouseIsPressed)
+  rect(mouseX, mouseY,20,20,2);}
+  
